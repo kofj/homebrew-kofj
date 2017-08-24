a1fc54a5dea4d4f646958402f93f0483276d74f6
Frank Kung <kfanjian@gmail.com>
2017-08-24 21:00:03 +0800

    Create formula.

---
+++ Formula/v2ray-core.rb
@@ -0,0 +1,41 @@
+class V2rayCore < Formula
+  desc "A platform for building proxies to bypass network restrictions."
+  homepage "https://www.v2ray.com/"
+  url "https://github.com/v2ray/v2ray-core/releases/download/v2.34/v2ray-macos.zip"
+  version "v2.34"
+  sha256 "a5ca67b7480a91682a1c7483f54e30d3cba521d7ca8335e8a869ee4bd2e87116"
+
+  def install
+    bin.install "v2ray"
+
+    etc.install "config.json"
+  end
+
+  plist_options :manual => "v2ray -config=#{HOMEBREW_PREFIX}/etc/config.json"
+
+  def plist; <<-EOS.undent
+  <?xml version="1.0" encoding="UTF-8"?>
+  <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+  <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <true/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{bin}/v2ray</string>
+        <string>-config</string>
+        <string>#{etc}/config.json</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+    </dict>
+  </plist>
+  EOS
+end
+  
+  test do
+    system "#{bin}/v2ray", "-version"
+  end
+end
